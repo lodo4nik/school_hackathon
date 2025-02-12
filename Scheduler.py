f90065e914942ae56ee1cd7a0615817059cb24e0
WoWonderdev <81707364+WoWonderdev@users.noreply.github.com>
2025-02-12 13:43:11 +0300

    Update Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -11,19 +11,6 @@ def create_json_file(filename, data):
         json.dump(data, json_file, indent=4, ensure_ascii=False)
 
     print(f"FILE CREATED: {file_path}")
-
-data = {
-    "tag": "хотелки денчика",
-    "name": "Сдать проект",
-    "description": "Сдать заявку на инженеры будущего чтоб денчик не убил своим пузом",
-    "task_begin": datetime(2025, 2, 10, 12, 00).isoformat() + "Z",
-    "deadline": datetime(2025, 2, 14, 19, 00).isoformat() + "Z",
-    "priority": 2,
-    "repetition": True,
-    "repetition_cooldown": 1,
-    "color": "red"
-}
-
 # create_json_file("task.json", data)
 
 print('Нажмите T, чтобы открыть список задач\n' + 'Нажмите C, чтобы открыть календарь\n')
